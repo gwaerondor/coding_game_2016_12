ae43d0e1373f28f99309f3ec523875716592e9fd
Robin Larsson <robin.larsson@ericsson.com>
2016-12-14 17:19:25 +0100

    Adds interval merging functionality

--- src/interval_merging.erl
+++ src/interval_merging.erl
@@ -1,2 +1,17 @@
 -module(interval_merging).
+-export([merge/1, are_overlapping/2]).
 
+merge(Short_list) when length(Short_list) < 2 ->
+    Short_list;
+merge([L = {L_min, _}, R = {_, R_max} | Rest]) ->
+    case are_overlapping(L, R) of
+	true ->
+	    Merged = {L_min, R_max},
+	    merge([Merged | Rest]);
+	false ->
+	    [L | merge([R | Rest])]
+    end.
+
+are_overlapping({Left_min, Left_max}, {Right_min, Right_max}) ->
+    Right_min =< Left_max andalso Right_max >= Left_min.
+	
--- src/interval_merging_tests.erl
+++ src/interval_merging_tests.erl
@@ -18,3 +18,26 @@ third_test() ->
     Expected = [{1,14}],
     Actual = interval_merging:merge(L),
     ?assertEqual(Expected, Actual).
+
+are_overlapping_test_() ->
+    [fun left_max_lower_than_right_min_no_overlap/0,
+     fun left_max_higher_than_right_min_overlaps/0,
+     fun left_max_and_min_higher_than_right_min_no_overlap/0].
+
+left_max_lower_than_right_min_no_overlap() ->
+    L = {1, 7},
+    R = {8, 9},
+    Result = interval_merging:are_overlapping(L, R),
+    ?assertNot(Result).
+
+left_max_higher_than_right_min_overlaps() ->
+    L = {1, 10},
+    R = {5, 15},
+    Result = interval_merging:are_overlapping(L, R),
+    ?assert(Result).
+
+left_max_and_min_higher_than_right_min_no_overlap() ->
+    L = {1000, 1001},
+    R = {1, 2},
+    Result = interval_merging:are_overlapping(L, R),
+    ?assertNot(Result).
